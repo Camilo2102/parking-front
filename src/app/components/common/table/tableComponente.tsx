527a89ca5518d58c28f3bf5b937f2d3e42559590
Jazmin Saleh <110407443+jazminasaleh@users.noreply.github.com>
2023-12-02 12:58:15 -0500

    tabla general, ya se edita y elimina datos

--- src/app/components/common/table/tableComponente.tsx
+++ src/app/components/common/table/tableComponente.tsx
@@ -1,19 +1,89 @@
-import { DataTable } from 'primereact/datatable';
+import React, { useEffect, useState } from 'react';
+import { DataTable, DataTableRowEditCompleteEvent } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { TableProps } from '@/app/interfaces/model';
+import Swal from 'sweetalert2'
 
+import { InputText } from 'primereact/inputtext';
+import 'primeicons/primeicons.css';
 
-export default function Table(props: TableProps) {
-    const tableColumns = ()=>{
-        return props.columns.map((column, index) =>
-            <Column key={"column-" + index} field={column.field} header={column.name} style={{ width: '20%' }}></Column>
-        )
-        
-    }
+interface TableItem {
+    id: string;
+    [key: string]: string;
+}
+
+interface TableProps<T> {
+    values: T[];
+    columns: { field: string; name: string }[];
+}
+
+export default function Table<T extends TableItem>(props: TableProps<T>) {
+    const [data, setData] = useState<T[]>([]);
+
+    useEffect(() => {
+        setData(props.values);
+    }, [props.values]);
+
+    const onRowEditComplete = (e: DataTableRowEditCompleteEvent) => {
+        let newData = [...data];
+        let { newData: editedData, index } = e;
+
+        newData[index] = editedData as T;
+        setData(newData);
+        Swal.fire('Editado', 'Los datos han sido editados con éxito.', 'success');
+    };
+
+    const textEditor = (options: any) => {
+        return <InputText type="text" value={options.value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => options.editorCallback(e.target.value)} />;
+    };
+
+    const deleteItem = (itemId: string) => {
+        const itemToDelete = data.find((item) => item.id === itemId);
+
+        if (itemToDelete) {
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: '¡No podrás revertir esto!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, eliminarlo',
+                cancelButtonText: 'Cancelar',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    const updatedData = data.filter((item) => item.id !== itemId);
+                    setData(updatedData);
+    
+                    Swal.fire('Eliminado', 'Los datos han sido eliminados.', 'success');
+                }
+            });
+        }
+    };
+
+    const tableColumns = () => {
+        return props.columns.map((column, index) => (
+            <Column
+                key={"column-" + index}
+                field={column.field}
+                header={column.name}
+                style={{ width: '20%' }}
+                headerStyle={{ fontWeight: 'bold' }}
+                editor={(options) => textEditor(options)}
+            />
+        ));
+    };
+
+    const deleteButton = (rowData: T) => (
+        <button className="p-button p-button-danger p-button-icon-only" onClick={() => deleteItem(rowData.id)}>
+            <i className="pi pi-trash"></i>
+        </button>
+    );
+
     return (
-            <DataTable value={props.values} editMode="row" dataKey="id" tableStyle={{ minWidth: '50rem' }}>
-                {tableColumns()}
-                <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
-            </DataTable>
-    )
+        <DataTable value={data} editMode="row" dataKey="id" tableStyle={{ minWidth: '50rem', border: '1px solid #ddd' }} style={{ margin: '5%' }} onRowEditComplete={onRowEditComplete}>
+            {tableColumns()}
+            <Column header="Editar" rowEditor headerStyle={{ width: '10%', minWidth: '8rem', fontWeight: 'bold' }}></Column>
+            <Column header="Eliminar" body={(rowData) => deleteButton(rowData)} headerStyle={{ width: '10%', minWidth: '8rem', fontWeight: 'bold' }}></Column>
+        </DataTable>
+    );
 }
--- src/app/pages/main/page.tsx
+++ src/app/pages/main/page.tsx
@@ -6,16 +6,21 @@ import { ColumnTable } from "@/app/interfaces/model";
 export default function Login() {
     const columns: ColumnTable[] = [
         { field: 'name', name: "Nombre" },
+        { field: 'address', name: "Dirección" },
+        { field: 'number', name: "Número" },
     ];
 
     const data = [
-        {name: "hola"},
-        {name: "hola"},
-        {name: "hola"},
-        {name: "hola"},
+        {id: "1", name: "Jazmin", address: "Cra 10 # 10-78", number: "316304678"},
+        {id: "2", name: "Andrea", address: "Cra 14 # 10-78", number: "3163484678"},
+        {id: "3", name: "Camilo",  address: "Cra 13 # 10-78", number: "398304678"},
+        {id: "4", name: "Juan Esteban",  address: "Cra 17 # 10-78", number: "312204678"},
     ]
 
     return (
-        <Table columns={columns} values={data}></Table>     
+        <>
+            <h1 style={{textAlign: 'center', fontSize: '50px'}}>Clientes</h1>
+            <Table columns={columns} values={data}></Table> 
+        </>  
     )
 }
